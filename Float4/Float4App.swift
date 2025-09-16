b10dac03ab415045ccb0345f2d1c9c150db68951
Anantika <43288485+3luthe@users.noreply.github.com>
2025-09-16 12:17:45 -0700

    Initial Commit

---
+++ Float4/Float4App.swift
@@ -0,0 +1,17 @@
+//
+//  Float4App.swift
+//  Float4
+//
+//  Created by Anantika Mannby on 9/16/25.
+//
+
+import SwiftUI
+
+@main
+struct Float4App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Float4Tests/Float4Tests.swift
@@ -0,0 +1,17 @@
+//
+//  Float4Tests.swift
+//  Float4Tests
+//
+//  Created by Anantika Mannby on 9/16/25.
+//
+
+import Testing
+@testable import Float4
+
+struct Float4Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
